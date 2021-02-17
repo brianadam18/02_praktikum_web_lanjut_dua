3e977d7c41b80ae302996b0d46b466e8b7130c71
= <=>
2021-02-17 11:55:32 +0700

    Finish : Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about (){
+        return "NIM : 1941720190,Nama : Brian Adam Bhagaskara";
+         ;
+}
+
+}
